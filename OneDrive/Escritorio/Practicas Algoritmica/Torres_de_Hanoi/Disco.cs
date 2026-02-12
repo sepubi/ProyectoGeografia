47d8503e6c87eb59c2b4c1eb30e396c8b4676acd
sepubi <160240637+sepubi@users.noreply.github.com>
2026-02-12 11:42:59 +0100

    Clase Disco Hecha

---
+++ OneDrive/Escritorio/Practicas Algoritmica/Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+        class Disco
+        {
+            public int Valor { get; set; } // Tamaño del disco
+
+            public Disco(int valor)
+            {
+                Valor = valor;
+            }
+
+            public override string ToString()
+            {
+            return Valor.ToString();
+            }
+        }
+    }
